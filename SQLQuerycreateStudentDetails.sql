223ddbfa2e9f6ccde5e22113f93a02390de8afe9
sravanichinthalapudi36 <68219520+sravanichinthalapudi36@users.noreply.github.com>
2020-08-14 13:47:36 +0530

    Update SQLQuerycreateStudentDetails.sql

--- SQLQuerycreateStudentDetails.sql
+++ SQLQuerycreateStudentDetails.sql
@@ -4,14 +4,3 @@ Studid  int primary key identity(100,1),
 StudName varchar(20),
 StudMarks int
 );
-
-use DotNetData
-create table DeptDetailsr(
-Deptid int identity(50,1),
-DeptName varchar(20),
-DeptStatus varchar(20)
-);
-
-       SET IDENTITY_INSERT DeptDetails ON
-
-	   select * from DeptDetailsr
